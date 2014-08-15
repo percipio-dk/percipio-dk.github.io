b49d2fc114e8d1a534620a252a62f0fcbe7fa219
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2014-08-15 07:46:18 +0200

    Added globe image.

--- js/percipio.js
+++ js/percipio.js
@@ -1,12 +1,9 @@
-﻿// create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('webmap').setView([55.715356, 12.549946], 17);
+﻿var map = L.map('webmap').setView([55.715356, 12.549946], 17);
 
-// add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// add a marker in the given location, attach some popup content to it and open the popup
 L.marker([55.715356, 12.549946]).addTo(map)
     .bindPopup('<p><strong class="percipio_blue">Percipio</strong>' + '<br/>' +
     'Fruebjergvej 3' + '<br/>' +
