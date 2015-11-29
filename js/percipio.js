050b5c0ed2eae943b5a0c0de37e340d32919b20a
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2015-11-29 19:34:31 +0100

    Changed address.

--- js/percipio.js
+++ js/percipio.js
@@ -1,13 +1,12 @@
-﻿var map = L.map('webmap').setView([55.715356, 12.549946], 17);
+﻿var map = L.map('webmap').setView([55.7033442, 12.4652376], 17);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([55.715356, 12.549946]).addTo(map)
+L.marker([55.7033442, 12.4652376]).addTo(map)
     .bindPopup('<p><strong class="percipio_blue">Percipio</strong>' + '<br/>' +
-    'Fruebjergvej 3' + '<br/>' +
-    '2100 K&oslash;benhavn &Oslash; <br/>' +
-    '<p/>' +
-    '<b>Åbningstider:</b> <br/>efter behov')
+    '&Aring;vendingen 24A' + '<br/>' +
+    '2700 Br&oslash;nsh&oslash;j <br/>' +
+    '<p/>')
     .openPopup();
